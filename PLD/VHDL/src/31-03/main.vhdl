f6b72994fdea5af768b33d3a7df7dd46fc571214
Oliver Pauer <83162717+tby73@users.noreply.github.com>
2023-03-31 10:12:56 +0200

    Update main.vhdl

--- PLD/VHDL/src/31-03/main.vhdl
+++ PLD/VHDL/src/31-03/main.vhdl
@@ -14,18 +14,19 @@ end HalfAdder;
 -- LOGIC
 architecture Behavioral of HalfAdder is
 begin
-    -- Method 1: basic AND logic
+    -- Method 1 (CO): basic AND logic
     carry_out <= a and b; 
 
+    -- Method 2 (CO): as process with SL-list
     process(a, b)
     begin
         carry_out <= a and b; 
     end process
 
-    -- Method 1: basic XOR logic
+    -- Method 1 (SUM): basic XOR logic
     sum <= a xor b; 
 
-    -- Method 2: conditional
+    -- Method 2 (SUM): conditional
     process(a, b)
     begin
         if (a = b)
@@ -35,4 +36,3 @@ begin
         end if
     end process
 end Behavioral;
-
